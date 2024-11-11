d47f5d0f019b2cf195e02a16c547e3bd457d9a01
habijung <habijung0@gmail.com>
2024-11-11 11:17:26 +0900

    Create simple fraction class

--- proj1/proj1/main.m
+++ proj1/proj1/main.m
@@ -7,10 +7,54 @@
 
 #import <Foundation/Foundation.h>
 
-int main(int argc, const char * argv[]) {
+
+@interface Fraction : NSObject
+
+- (void) print;
+- (void) setNumerator: (int) n;
+- (void) setDenominator: (int) d;
+
+@end
+
+
+@implementation Fraction
+{
+    int numerator;
+    int denominator;
+}
+
+- (void)print
+{
+    NSLog (@"%i/%i", numerator, denominator);
+}
+
+- (void)setNumerator: (int) n
+{
+    numerator = n;
+}
+
+- (void)setDenominator: (int) d
+{
+    denominator = d;
+}
+
+@end
+
+
+int main(int argc, const char * argv[])
+{
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        Fraction *myFraction;
+        
+        myFraction = [Fraction alloc];
+        myFraction = [myFraction init];
+        
+        [myFraction setNumerator: 1];
+        [myFraction setDenominator: 3];
+        
+        NSLog(@"The value of myFraction is: ");
+        [myFraction print];
     }
+
     return 0;
 }
