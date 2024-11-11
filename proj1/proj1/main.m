8dabdfa75d11e9e5ba3cbdb0174b88d04390a9e9
habijung <habijung0@gmail.com>
2024-11-11 11:39:31 +0900

    Create integer getter methods

--- proj1/proj1/main.m
+++ proj1/proj1/main.m
@@ -13,6 +13,8 @@
 - (void) print;
 - (void) setNumerator: (int) n;
 - (void) setDenominator: (int) d;
+- (int) numerator;
+- (int) denominator;
 
 @end
 
@@ -23,19 +25,29 @@
     int denominator;
 }
 
-- (void)print
+- (void) print
 {
     NSLog (@"%i/%i", numerator, denominator);
 }
 
-- (void)setNumerator: (int) n
+- (void) setNumerator: (int) n
 {
     numerator = n;
 }
 
-- (void)setDenominator: (int) d
+- (void) setDenominator: (int) d
 {
     denominator = d;
+}
+
+- (int) numerator
+{
+    return numerator;
+}
+
+- (int) denominator
+{
+    return denominator;
 }
 
 @end
@@ -46,14 +58,13 @@ int main(int argc, const char * argv[])
     @autoreleasepool {
         Fraction *myFraction;
         
-        myFraction = [Fraction alloc];
-        myFraction = [myFraction init];
+        myFraction = [[Fraction alloc] init];
+        // myFraction = [Fraction new];
         
         [myFraction setNumerator: 1];
         [myFraction setDenominator: 3];
         
-        NSLog(@"The value of myFraction is: ");
-        [myFraction print];
+        NSLog(@"The value of myFraction is: %i/%i", [myFraction numerator], [myFraction denominator]);
     }
 
     return 0;
