2af320918eacdc3d31ed28b981d1503ab7bde975
habijung <habijung0@gmail.com>
2024-11-11 13:47:39 +0900

    Test for loop with scanf

--- proj1/proj1/main.m
+++ proj1/proj1/main.m
@@ -8,63 +8,22 @@
 #import <Foundation/Foundation.h>
 
 
-@interface Fraction : NSObject
-
-- (void) print;
-- (void) setNumerator: (int) n;
-- (void) setDenominator: (int) d;
-- (int) numerator;
-- (int) denominator;
-
-@end
-
-
-@implementation Fraction
-{
-    int numerator;
-    int denominator;
-}
-
-- (void) print
-{
-    NSLog (@"%i/%i", numerator, denominator);
-}
-
-- (void) setNumerator: (int) n
-{
-    numerator = n;
-}
-
-- (void) setDenominator: (int) d
-{
-    denominator = d;
-}
-
-- (int) numerator
-{
-    return numerator;
-}
-
-- (int) denominator
-{
-    return denominator;
-}
-
-@end
-
-
 int main(int argc, const char * argv[])
 {
     @autoreleasepool {
-        Fraction *myFraction;
+        int n, number, triangularNumber;
         
-        myFraction = [[Fraction alloc] init];
-        // myFraction = [Fraction new];
+        NSLog(@"What triangular number do you want?");
+        scanf("%i", &number);
         
-        [myFraction setNumerator: 1];
-        [myFraction setDenominator: 3];
+        triangularNumber = 0;;
         
-        NSLog(@"The value of myFraction is: %i/%i", [myFraction numerator], [myFraction denominator]);
+        for (n = 1; n <= number; ++n)
+        {
+            triangularNumber += n;
+        }
+        
+        NSLog(@"Triangular number %i is %i\n", number, triangularNumber);
     }
 
     return 0;
