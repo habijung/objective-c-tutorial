cb354817aa249b168f75a924ef95ebdbb678f16d
habijung <habijung0@gmail.com>
2024-11-11 10:20:20 +0900

    Add new project: proj1

---
+++ proj1/proj1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  proj1
+//
+//  Created by 정하빈 on 11/11/24.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
