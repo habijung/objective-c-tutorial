f5aa353b643f2b14821b96acfd405fd8b872a415
habijung <habijung0@gmail.com>
2024-11-11 14:14:57 +0900

    Fix typo

--- proj1/proj1/main.m
+++ proj1/proj1/main.m
@@ -16,7 +16,7 @@ int main(int argc, const char * argv[])
         NSLog(@"What triangular number do you want?");
         scanf("%i", &number);
         
-        triangularNumber = 0;;
+        triangularNumber = 0;
         
         for (n = 1; n <= number; ++n)
         {
