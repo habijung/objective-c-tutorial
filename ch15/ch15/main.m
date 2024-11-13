a2437efd6dc2d5952d4a60ad03ca42c84fe28fcd
habijung <habijung0@gmail.com>
2024-11-13 10:30:20 +0900

    Create new project: ch15

---
+++ ch15/ch15/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ch15
+//
+//  Created by 정하빈 on 11/13/24.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
