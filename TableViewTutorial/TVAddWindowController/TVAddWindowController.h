bac9c92c089474a5dd284e748add9a63a5d82f17
habijung <habijung0@gmail.com>
2024-11-26 11:35:08 +0900

    Create new window controller: TVAddWindowController

---
+++ TableViewTutorial/TVAddWindowController/TVAddWindowController.h
@@ -0,0 +1,16 @@
+//
+//  TVAddWindowController.h
+//  TableViewTutorial
+//
+//  Created by 정하빈 on 11/20/24.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TVAddWindowController : NSWindowController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TableViewTutorial/TVAddWindowController/TVAddWindowController.m
@@ -0,0 +1,22 @@
+//
+//  TVAddWindowController.m
+//  TableViewTutorial
+//
+//  Created by 정하빈 on 11/20/24.
+//
+
+#import "TVAddWindowController.h"
+
+@interface TVAddWindowController ()
+
+@end
+
+@implementation TVAddWindowController
+
+- (void)windowDidLoad {
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
