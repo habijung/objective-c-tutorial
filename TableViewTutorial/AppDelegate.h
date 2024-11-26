dab1df720e6f3e595d528d7dda6ecf224ec4794e
habijung <habijung0@gmail.com>
2024-11-26 11:35:08 +0900

    Create new project: TableViewTutorial

---
+++ TableViewTutorial/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  TableViewTutorial
+//
+//  Created by 정하빈 on 11/13/24.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ TableViewTutorial/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  TableViewTutorial
+//
+//  Created by 정하빈 on 11/13/24.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
