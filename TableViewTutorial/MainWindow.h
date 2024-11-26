60eca9ad44d34fed4e5e1e9d9b82e3fc5a54338e
habijung <habijung0@gmail.com>
2024-11-26 11:35:08 +0900

    Create buttons outlet and action (Add, Show, Delete)

---
+++ TableViewTutorial/MainWindow.h
@@ -0,0 +1,26 @@
+//
+//  MainWindow.h
+//  TableViewTutorial
+//
+//  Created by 정하빈 on 11/18/24.
+//
+
+#import <Foundation/Foundation.h>
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MainWindow : NSObject
+{
+    __weak IBOutlet NSButton *addButton;
+    __weak IBOutlet NSButton *showButton;
+    __weak IBOutlet NSButton *deleteButton;
+}
+
+- (IBAction)addButtonClicked:(NSButton *)sender;
+- (IBAction)showButtonClicked:(NSButton *)sender;
+- (IBAction)deleteButtonClicked:(NSButton *)sender;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TableViewTutorial/MainWindow.m
@@ -0,0 +1,27 @@
+//
+//  MainWindow.m
+//  TableViewTutorial
+//
+//  Created by 정하빈 on 11/18/24.
+//
+
+#import "MainWindow.h"
+
+@implementation MainWindow
+
+- (IBAction)addButtonClicked:(NSButton *)sender
+{
+    NSLog(@"addButtonClicked");
+}
+
+- (IBAction)showButtonClicked:(NSButton *)sender
+{
+    NSLog(@"showButtonClicked");
+}
+
+- (IBAction)deleteButtonClicked:(NSButton *)sender
+{
+    NSLog(@"deleteButtonClicked");
+}
+
+@end
