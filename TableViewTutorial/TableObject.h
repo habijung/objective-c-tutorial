5bc11cd37ffadc142ff4b9ebec31c174d61fbf01
habijung <habijung0@gmail.com>
2024-11-26 11:35:08 +0900

    Create class TableObject

---
+++ TableViewTutorial/TableObject.h
@@ -0,0 +1,16 @@
+//
+//  TableObject.h
+//  TableViewTutorial
+//
+//  Created by 정하빈 on 11/18/24.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TableObject : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TableViewTutorial/TableObject.m
@@ -0,0 +1,12 @@
+//
+//  TableObject.m
+//  TableViewTutorial
+//
+//  Created by 정하빈 on 11/18/24.
+//
+
+#import "TableObject.h"
+
+@implementation TableObject
+
+@end
